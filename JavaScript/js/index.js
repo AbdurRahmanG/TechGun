14bbc478a61d7d7eb0b014d426717c8035e2c301
AbdurRahman G <abdurrahmangofficial@gmail.com>
2022-11-03 10:07:44 +0530

    Learnt Statements and Comments

--- JavaScript/js/index.js
+++ JavaScript/js/index.js
@@ -2,3 +2,19 @@ document.getElementById("hello").innerHTML = "Salam AbdurRahman G";
 document.write("Again Salam AbdurRahman G");
 alert("Salam AbdurRahman G !!!");
 console.log("Salam AbdurRahman G!")
+
+document.write("Now learnt Statements and Comments")
+
+/* 
+Statements are basically the instructions which tell the computer exactly what to do
+*/
+
+/*
+Comments are ignorable statements for JavaScript. These all statements will be ignored by JS
+*/
+// Single line comment
+/*
+Multiple line comment
+Multiple line comment 
+*/
+

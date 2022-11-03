64607e2d377d6ce045e215b42fb2be51141fe4c3
AbdurRahman G <abdurrahmangofficial@gmail.com>
2022-11-03 09:44:55 +0530

    Initial Commit

---
+++ JavaScript/js/index.js
@@ -0,0 +1,4 @@
+document.getElementById("hello").innerHTML = "Salam AbdurRahman G";
+document.write("Again Salam AbdurRahman G");
+alert("Salam AbdurRahman G !!!");
+console.log("Salam AbdurRahman G!")
